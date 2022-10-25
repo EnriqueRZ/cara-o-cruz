41275fd5755574defc3985aa2cedd67cf24b31a8
EnriqueRZ <je.ramirez.zuniga@gmail.com>
2022-10-25 16:37:34 -0500

    Adding random function generator, this function generate float random number betwen 0 and 1

---
+++ utils/random_generator.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"crypto/rand"
+	"math/big"
+);
+
+func RandIntn(max int64) int64 {
+	nBig, err := rand.Int(rand.Reader, big.NewInt(max))
+	if err != nil {
+		panic(err)
+	}
+	return nBig.Int64()
+}
