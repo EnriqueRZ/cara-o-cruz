eb247ab1d2843143025a699f2ae77b32f4a6ae8e
EnriqueRZ <je.ramirez.zuniga@gmail.com>
2022-10-26 22:13:39 -0500

    Adding a first try side of the coin and funtion to clear the terminal

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"cara-cruz/utils"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
 );
 
 func main() {
 	random := float64(utils.RandIntn(1<<53)) / (1 << 53)
 	fmt.Print("RANDOM = ", random);
+	
+	carasArray := utils.GetCaras()
+	for _, s := range carasArray {
+		fmt.Printf("%2s .\r", s)
+		time.Sleep(1 * time.Second)
+		clearScreen()
+	}
+}
+
+var clearScreen = func() {
+	fmt.Print("os ", runtime.GOOS)
+    cmd := exec.Command("clear") // clears the scrollback buffer as well as the screen.
+    cmd.Stdout = os.Stdout
+    cmd.Run()
 }
